87742199f5316b320d5df76389a61ab02fea6107
Sinsurin Jacques <sinsurin.jacques1994@gmail.com>
2026-08-28 14:31:26 -0400

    Refresh Clean Core alpha.13 cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION='2.0.0-alpha.12';
+const APP_VERSION='2.0.0-alpha.13';
 const STATIC_CACHE=`chebsel-v2-static-${APP_VERSION}`;
 const HTML_CACHE=`chebsel-v2-html-${APP_VERSION}`;
 const APP_SHELL=[
